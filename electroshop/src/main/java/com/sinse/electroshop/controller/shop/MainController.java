cb53b034903b2604e6ad8b2fb5244e3e1dac7c02
lecturepc <devlearncampus@gmail.com>
2025-08-25 16:36:37 +0900

    금일수업

--- electroshop/src/main/java/com/sinse/electroshop/controller/shop/MainController.java
+++ electroshop/src/main/java/com/sinse/electroshop/controller/shop/MainController.java
@@ -1,11 +1,11 @@
-package com.sinse.electroshop.controller;
+package com.sinse.electroshop.controller.shop;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-    @GetMapping("/")
+    @GetMapping("/shop/main")
     public String index() {
         return "electro/index";
     }
