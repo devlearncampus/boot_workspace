1d57b02d077d4d95ca33fdcad58517d6e65304ed
lecturepc <devlearncampus@gmail.com>
2025-09-05 10:06:40 +0900

    금일수업

--- JwtLogin/src/main/java/com/sinse/jwtlogin/security/SecurityConfig.java
+++ JwtLogin/src/main/java/com/sinse/jwtlogin/security/SecurityConfig.java
@@ -1,9 +1,13 @@
 package com.sinse.jwtlogin.security;
 
 
+import com.sinse.jwtlogin.model.member.CustomUserDetailsService;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnNotWarDeployment;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -16,17 +20,47 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    //현재 db에 들어간 비밀번호는 BCrypt 알고리즘이므로, 시큐리티에게 같은 알고리즘을 사용하도
-    //록 알려줘야 함
+    //시큐리티가 사용할 서비스 객체 등록
+    private final CustomUserDetailsService customUserDetailsService;
+
+    public SecurityConfig(CustomUserDetailsService customUserDetailsService) {
+        this.customUserDetailsService = customUserDetailsService;
+    }
+
+    //DaoAuthenticationProvider가 사용할 비번 검증 인코더 등록
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(); //비번결과물 자체에 salt 가 포함됨
+    }
+
+    //아이디와 패스워드를 자동으로 비교해주는 프로바이더 등록
+    @Bean
+    public DaoAuthenticationProvider daothenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        //프로바이더가 사용할 서비스 객체 등록
+        provider.setUserDetailsService(customUserDetailsService);
+
+        //프로바이더가 사용할 비번 인코더
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                //스프링시큐리티가 기본적으로 CRSF 방지기능이 있음 , 아래의 코드로 disable  시키면 
+                //CSRF를 비활성화시킴 - 전통적인 form 로그인/세션 방식이 아니라 REST/JWT 로 인증을 처리할때는
+                //보통 끔
                 .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(form -> form
+                        .loginPage("/member/login.html") //로그인 폼의 접근 url
+                        .loginProcessingUrl("/member/login") //시큐리티가 로그인일 처리할 url
+                )
                 .build();
     }
 
